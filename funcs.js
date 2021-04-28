fb5aad3759dbc8168c037be9dbc967a430ec308b
mkarkaus <miikka.karkaus@gmail.com>
2021-04-28 17:29:21 +0300

    Fixed GitHub project list, added smooth animations

--- funcs.js
+++ funcs.js
@@ -38,35 +38,71 @@ myname.style.fontSize = "8vh";
 while (window.innerWidth * 0.9 < myname.clientWidth)
 	myname.style.fontSize = (parseFloat(myname.style.fontSize) - 0.1) + "vh";
 
-
-function thatsMe(id)
-{
-	var name;
+$(document).ready(function() {
 	
-	name = document.getElementById(id);
-
-	name.style.animation = "fadeAway 0.5s";
-	name.style.visibility = "hidden";
-	setTimeout(function() {
-		name.style.animation = "moveFromTop 1s";
-		name.style.visibility = "visible";
-		name.innerHTML = "That's me!";
-		name.style.margin = "-10vh auto auto auto";
-	}, 500);
-	setTimeout(function() {backToMiikka(id);}, 2000);
-};
-
-function backToMiikka(id)
-{
-	var name;
+	$('#name').click(function() {
 	
-	name = document.getElementById(id);
-	name.style.animation = "fadeAway 0.5s";
-	name.style.visibility = "hidden";
-	setTimeout(function() {
-		name.style.animation = "moveFromTop2 1s";
-		name.style.visibility = "visible";
-		name.innerHTML = "MIIKKA KARKAUS";
-		name.style.margin = "-10vh auto auto auto";
-	}, 500);
-};
+		$('#name').css("animation", "fadeAway 0.5s");
+		$('#name').css("visibility", "hidden");
+		setTimeout(function() {
+			$('#name').css("animation", "moveFromTop 1s");
+			$('#name').css("visibility", "visible");
+			$('#name').html("That's me!");
+			$('#name').css("margin", "-10vh auto auto auto");
+		}, 500);
+		setTimeout(function() {backToMiikka();}, 2000);
+	});
+	
+	function backToMiikka()
+	{
+		$('#name').css("animation", "fadeAway 0.5s");
+		$('#name').css("visibility", "hidden");
+		setTimeout(function() {
+			$('#name').css("animation", "moveFromTop2 1s");
+			$('#name').css("visibility", "visible");
+			$('#name').html("MIIKKA KARKAUS");
+			$('#name').css("margin", "-10vh auto auto auto");
+		}, 500);
+	};
+
+	var	projects = ["Lem_in", "Filler", "ft_printf", "Fillit", "Libft"];
+
+	$('.idgithub').mouseenter(function() {
+		/* Open the list of projects */
+	
+		$.each(projects, function(index, value) {
+			var	url = "https://github.com/mkarkaus/" + value;
+			var pad = $('.idgithub').css('padding-top');
+
+			$('<div/>')
+			.css({'font-size':parseInt(pad) * 2,
+					'padding-top':pad,
+					'padding-bottom':pad,
+					'text':value,
+					'opacity':'0'})
+			.attr('class', 'github-list rm_list')
+			.attr('id', value)
+			.html('<a href="' + url + '">' + value + '</a>')
+			.appendTo('#github');
+			$('#' + value).animate({
+				opacity: 1,
+				top: parseInt($('.idgithub').css('height')) + 'px'
+			}, (index + 1) * 150);
+		});
+	});
+
+	$('.rm_list').mouseleave(function() {
+		/* Close the list of projects */
+		$()
+			$.each(projects, function(index, value) {
+				console.log(index + ' ' + value);
+				$('#' + value).animate({
+					opacity: 0,
+					top: 0
+				}, (index + 1) * 150);
+				setTimeout(function () {
+					$('#' + value).remove();
+				}, 5 * 150);
+			});
+	});
+});
