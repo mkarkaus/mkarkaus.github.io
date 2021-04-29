e9b2d633e57282f77f370c854f221aabeb58f171
mkarkaus <miikka.karkaus@gmail.com>
2021-04-29 15:21:32 +0300

    added a floating arrow and fixed some positions on the projects

--- funcs.js
+++ funcs.js
@@ -39,7 +39,7 @@ while (window.innerWidth * 0.9 < myname.clientWidth)
 	myname.style.fontSize = (parseFloat(myname.style.fontSize) - 0.1) + "vh";
 
 $(document).ready(function() {
-	
+
 	$('#name').click(function() {
 	
 		$('#name').css("animation", "fadeAway 0.5s");
@@ -69,7 +69,13 @@ $(document).ready(function() {
 
 	$('.idgithub').mouseenter(function() {
 		/* Open the list of projects */
-	
+
+		$('#arrow').animate({
+			opacity: 0,
+			top: '200px'
+		}, 250, () => {
+			$('#arrow').css('top', 0);
+		});
 		$.each(projects, function(index, value) {
 			var	url = "https://github.com/mkarkaus/" + value;
 			var pad = $('.idgithub').css('padding-top');
@@ -78,6 +84,7 @@ $(document).ready(function() {
 			.css({'font-size':parseInt(pad) * 2,
 					'padding-top':pad,
 					'padding-bottom':pad,
+					'top':-pad,
 					'text':value,
 					'opacity':'0'})
 			.attr('class', 'github-list rm_list')
@@ -86,23 +93,27 @@ $(document).ready(function() {
 			.appendTo('#github');
 			$('#' + value).animate({
 				opacity: 1,
-				top: parseInt($('.idgithub').css('height')) + 'px'
+				top: '-10px'
+				// top: parseInt($('.idgithub').css('height')) + 'px'
 			}, (index + 1) * 150);
 		});
 	});
 
 	$('.rm_list').mouseleave(function() {
 		/* Close the list of projects */
-		$()
-			$.each(projects, function(index, value) {
-				console.log(index + ' ' + value);
-				$('#' + value).animate({
-					opacity: 0,
-					top: 0
-				}, (index + 1) * 150);
-				setTimeout(function () {
-					$('#' + value).remove();
-				}, 5 * 150);
-			});
+		$.each(projects, function(index, value) {
+			console.log(index + ' ' + value);
+			$('#' + value).animate({
+				opacity: 0,
+				top: 0
+			}, (index + 1) * 150);
+			setTimeout(function () {
+				$('#' + value).remove();
+			}, 5 * 150);
+		});
+		$('#arrow').animate({
+			opacity: 1,
+			top: '10px'
+		}, 1000);
 	});
 });
